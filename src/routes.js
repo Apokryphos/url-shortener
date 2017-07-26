cb4900c9c2d1dda8893f67db0198a22aa90b26ed
Tony Slavotinek <apokryphos@users.noreply.github.com>
2017-07-26 15:03:58 -0400

    Removed some console messages and commented code.

--- src/routes.js
+++ src/routes.js
@@ -12,17 +12,14 @@ module.exports = function (app, db) {
     const longUrl = req.query.url;
 
     if (urlApi.longUrlIsValid(longUrl)) {
-      console.log('Received valid URL...');
       urlApi.shortenUrl(db, longUrl)
         .then(shortUrl => {
-          console.log('Sent short URL...');
           res.json({
             longUrl,
             shortUrl,
           });
         })
         .catch(err => {
-          console.log('ERROR short URL...');
           console.error(err);
           res.status(400).json({
             longUrl: null,
@@ -40,11 +37,9 @@ module.exports = function (app, db) {
   app.get('/:shortUrl', (req, res) => {
     const shortUrl = req.params.shortUrl;
 
-    console.log(`Received short URL ${shortUrl}...`);
     urlApi.getLongUrl(db, shortUrl)
       .then(longUrl => {
         if (longUrl) {
-          console.log(`Redirecting to ${longUrl}...`);
           return res.redirect(302, longUrl);
         } else {
           res.status(404).sendFile(path.join(__dirname, '../public/404.html'));
